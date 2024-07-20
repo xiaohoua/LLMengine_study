aec1f39d83de02d7cd6c73950d1d0ba92af067a2
xiaohoua <2889608379@qq.com>
2024-07-20 07:10:44 +0000

    embedding_weights.h macro.h string_utils.h

---
+++ src/utils/string_utils.h
@@ -0,0 +1,29 @@
+template<typename T>
+inline std::string vec2str(std::vector<T> vec)
+{
+    std::stringstream ss;
+    ss << "(";
+    if (!vec.empty()) {
+        for (size_t i = 0; i < vec.size() - 1; ++i) {
+            ss << vec[i] << ", ";
+        }
+        ss << vec.back();
+    }
+    ss << ")";
+    return ss.str();
+}
+
+template<typename T>
+inline std::string arr2str(T* arr, size_t size)
+{
+    std::stringstream ss;
+    ss << "(";
+    for (size_t i = 0; i < size - 1; ++i) {
+        ss << arr[i] << ", ";
+    }
+    if (size > 0) {
+        ss << arr[size - 1];
+    }
+    ss << ")";
+    return ss.str();
+}
---
+++ src/weights/llama/embedding_weights.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "src/weights/base_weights.h"
+
+template<typename T>
+struct EmbeddingWeight:public BaseWeight<T>{
+ 
+};
