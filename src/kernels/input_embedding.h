d007f53f50a43cc76fa009063970f8bcf32ea89b
xiaohoua <2889608379@qq.com>
2024-07-20 11:59:43 +0000

    test_input_embedding.cu

---
+++ src/kernels/input_embedding.h
@@ -0,0 +1,10 @@
+#include<cuda_runtime.h>
+#include<cuda.h>
+#include<cuda_fp16.h>
+#include"src/weights/llama/embedding_weights.h"
+#include"src/utils/tensor.h"
+
+template<typename T>
+void launchInputEmbedding(TensorWrapper<int>* input_ids,
+                            TensorWrapper<T>* output,
+                            EmbeddingWeight<T>* embed_table);
