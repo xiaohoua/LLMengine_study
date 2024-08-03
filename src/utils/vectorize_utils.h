a5223b4e20cdd66445407afa73f274dc8792de54
xiaohoua <2889608379@qq.com>
2024-08-03 06:44:41 +0000

    attn_softmax

--- src/utils/vectorize_utils.h
+++ src/utils/vectorize_utils.h
@@ -3,6 +3,40 @@
 #include <cuda_runtime.h>
 #include <cuda_fp16.h>
 
+template<typename T_OUT, typename T_IN>
+inline __device__ T_OUT scalar_cast_vec(T_IN val)
+{
+    return val;
+}
+
+template<>
+inline __device__ half2 scalar_cast_vec<half2, float>(float val)
+{
+    return __float2half2_rn(val);
+}
+
+template<>
+inline __device__ float4 scalar_cast_vec<float4, float>(float val)
+{
+    return make_float4(val, val, val, val);
+}
+
+template<>
+inline __device__ float2 scalar_cast_vec<float2, float>(float val)
+{
+    return make_float2(val, val);
+}
+
+template<>
+inline __device__ half2 scalar_cast_vec<half2, half>(half val)
+{
+    //(RussWong)note: __half2half2 cant be parsed by my nvcc compiler, so I give it up
+    //return __half2half2(val);
+    half2 res;
+    res.x = val;
+    res.y = val;
+    return res;
+}
 template<typename T>
 struct Vec {
     using Type = T;
